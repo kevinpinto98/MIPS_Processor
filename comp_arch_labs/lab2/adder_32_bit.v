38532c8684b6ecf7eae74c652911eb6f543bd6fd
Kevin Pinto <kevinpinto98@gmail.com>
2023-09-08 14:30:05 +0200

    lab2 changes

--- comp_arch_labs/lab2/adder_32_bit.v
+++ comp_arch_labs/lab2/adder_32_bit.v
@@ -9,9 +9,9 @@ input Cin;
 
 wire C0,C1,C2;
 
-adder_8_bit add1([7:0]S,C0,[7:0]A,[7:0]B,Cin);
-adder_8_bit add2([15:8]S,C1,[15:8]A,[15:8]B,C0);
-adder_8_bit add3([23:16]S,C2,[23:16]A,[23:16]B,C1);
-adder_8_bit add4([31:24]S,C,[31:24]A,[31:24]B,C2);
+adder_8_bit add1(S[7:0],C0,A[7:0],B[7:0],Cin);
+adder_8_bit add2(S[15:8],C1,A[15:8],B[15:8],C0);
+adder_8_bit add3(S[23:16],C2,A[23:16],B[23:16],C1);
+adder_8_bit add4(S[31:24],C,A[31:24],B[31:24],C2);
 
 endmodule
--- comp_arch_labs/lab2/tb_adder_32_bit.v
+++ comp_arch_labs/lab2/tb_adder_32_bit.v
@@ -10,7 +10,15 @@ reg Cin;
 adder_32_bit dut(S,C,A,B,Cin);
 
 initial begin
-
+   A = 32'd255;B = 32'd255; Cin = 1'b1;
+#5 A = 32'd64;B = 32'd102; Cin = 1'b0;
+#5 A = 32'd57;B = 32'd86; Cin = 1'b0;
+#5 A = 32'd46;B = 32'd177; Cin = 1'b1;
+#5 A = 32'd173;B = 32'd27; Cin = 1'b1;
+#5 A = 32'd200;B = 32'd21; Cin = 1'b1;
+#5 A = 32'd12;B = 32'd54; Cin = 1'b0;
+#5 A = 32'd224;B = 32'd77; Cin = 1'b1;
+#5 A = 32'd157;B = 32'd66; Cin = 1'b0;
 end
 
 initial begin
